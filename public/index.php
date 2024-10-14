899fc17c7d09d75ae748762785208d7aa0e12462
INMD1 <lyw514549@gmail.com>
2024-10-14 11:42:53 +0900

    asd

--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vite + Vue + TS</title>
-    <script type="module" crossorigin src="/assets/js/index.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/css/index.css">
+    <script type="module" crossorigin src="/php-vue/public/assets/js/index.js"></script>
+    <link rel="stylesheet" crossorigin href="/php-vue/public/assets/css/index.css">
   </head>
   <body>
     <div id="app"></div>
